a7a03cdd4c665344309d32808333c8ae01cb95ca
Robert van den Boom <vdboom.robert@gmail.com>
2025-02-27 16:41:03 +0100

    dish structure in JS

---
+++ script.js
@@ -0,0 +1,40 @@
+let allDishes = [
+    {'starters': [
+        {'dish': 'Pizza Margaritha', 'price': 7.9, 'description' : '', 'amount' : 0 },
+        {'dish': 'Pizza Tonno', 'price': 7.9, 'description' : '', 'amount' : 0 },
+        {'dish': 'Pizza Funghi', 'price': 7.9, 'description' : '', 'amount' : 0 },
+        {'dish': 'Pizza Caprese', 'price': 7.9, 'description' : '', 'amount' : 0 },
+        {'dish': 'Pizza Bolognese', 'price': 7.9, 'description' : '', 'amount' : 0 }
+
+    ]
+},
+{'main_dishes': [
+    {'dish': 'Pizza Margaritha', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Tonno', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Funghi', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Caprese', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Bolognese', 'price': 7.9, 'description' : '', 'amount' : 0 }
+
+]
+},
+{'deserts': [
+    {'dish': 'Pizza Margaritha', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Tonno', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Funghi', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Caprese', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Bolognese', 'price': 7.9, 'description' : '', 'amount' : 0 }
+
+]
+},
+{'beverages': [
+    {'dish': 'Pizza Margaritha', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Tonno', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Funghi', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Caprese', 'price': 7.9, 'description' : '', 'amount' : 0 },
+    {'dish': 'Pizza Bolognese', 'price': 7.9, 'description' : '', 'amount' : 0 }
+
+]
+}
+]
+
+let currency = {'euro': 1, 'dollar' : 1.04}
