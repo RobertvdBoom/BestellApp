22677cf8c034bda0b566ebad80be41ae00c5fb2f
Robert van den Boom <vdboom.robert@gmail.com>
2025-02-28 11:10:44 +0100

    test commit

--- script.js
+++ script.js
@@ -1,49 +1,57 @@
 let allDishes = [
     {
-        'starters': [
-            { 'dish': 'Pizza Margaritha', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Tonno', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Funghi', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Caprese', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Bolognese', 'price': 7.9, 'description': '', 'amount': 0 }
+        "starters": [
+            { "dish": "Pizza Margaritha", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Tonno", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Funghi", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Caprese", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Bolognese", "price": 7.9, "description": "", "amount": 0 }
 
         ]
     },
 
     {
-        'main_dishes': [
-            { 'dish': 'Pizza Margaritha', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Tonno', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Funghi', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Caprese', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Bolognese', 'price': 7.9, 'description': '', 'amount': 0 }
+        "main_dishes": [
+            { "dish": "Pizza Margaritha", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Tonno", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Funghi", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Caprese", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Bolognese", "price": 7.9, "description": "", "amount": 0 }
 
         ]
     },
 
     {
-        'deserts': [
-            { 'dish': 'Pizza Margaritha', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Tonno', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Funghi', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Caprese', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Bolognese', 'price': 7.9, 'description': '', 'amount': 0 }
+        "deserts": [
+            { "dish": "Pizza Margaritha", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Tonno", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Funghi", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Caprese", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Bolognese", "price": 7.9, "description": "", "amount": 0 }
 
         ]
     },
 
     {
-        'beverages': [
-            { 'dish': 'Pizza Margaritha', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Tonno', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Funghi', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Caprese', 'price': 7.9, 'description': '', 'amount': 0 },
-            { 'dish': 'Pizza Bolognese', 'price': 7.9, 'description': '', 'amount': 0 }
+        "beverages": [
+            { "dish": "Pizza Margaritha", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Tonno", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Funghi", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Caprese", "price": 7.9, "description": "", "amount": 0 },
+            { "dish": "Pizza Bolognese", "price": 7.9, "description": "", "amount": 0 }
 
         ]
     }
 ];
 
-const currency = { 'euro': 1, 'dollar': 1.04 };
+const currency = { "euro": 1, "dollar": 1.04 };
 
 let basket = [];
+
+function renderBasket() {
+    for (let index = 0; index < basket.length; index++) {
+        const item = basket[index];
+        let basketRef = document.getElementById('basket');
+        
+    }
+}
