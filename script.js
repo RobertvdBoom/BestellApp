58637a7522001bf3921fa6335a0bd6178a65b248
Robert van den Boom <vdboom.robert@gmail.com>
2025-02-28 23:43:50 +0100

    update and testing JSON functinos from module 7

--- script.js
+++ script.js
@@ -55,3 +55,45 @@ function renderBasket() {
         
     }
 }
+
+let outputRef = document.getElementById('output');
+
+let myObjectArr = [
+    {
+        "name":"Max",
+        "is_a_good_guy": true 
+    },
+    {
+        "name":"Peter",
+        "is_a_good_guy": false 
+    },
+    {
+        "name":"Arnold",
+        "is_a_good_guy": true 
+    },
+    {
+        "name":"Justus",
+        "is_a_good_guy": true 
+    },
+    {
+        "name":"Bombur",
+        "is_a_good_guy": false 
+    }
+];
+
+
+console.log(
+    myObjectArr.filter((element) => {return element["name"] != 'Bombur'})
+);
+
+console.log(
+    myObjectArr.findIndex((element) => {return element["name"] == "Justus"})
+);
+
+function checkName(name){
+    return name == "Justus";
+}
+
+console.log(
+    myObjectArr.findIndex(checkName)
+);
