aa53218605baeaa3ff6d36293c5a8f7ed3f83a89
Robert van den Boom <vdboom.robert@gmail.com>
2025-08-25 16:20:37 +0200

    database prep, final html structure for cards

---
+++ assets/js/db.js
@@ -0,0 +1,91 @@
+let dishData = [
+    {
+        "starters": [
+            {
+                "dishName": "Bruschetta",
+                "dishID": "s010001",
+                "dishPrice": 7.60,
+                "dishDescription": "Bruschetta",
+                "dishImageURL": "Bruschetta",
+                "dishImageAlt": "Insert Alt Text here",
+                "dishInfoForWACG": "Placeholder, in Case needed"
+            },
+
+            {
+                "dishName": "Pizzabrötchen mit Aioli",
+                "dishID": "s010002",
+                "dishPrice": 4.50,
+                "dishDescription": "Insert Dish description for p Tag here",
+                "dishImageURL": "Insert URL Here",
+                "dishImageAlt": "Insert Alt Text here",
+                "dishInfoForWACG": "Placeholder, in Case needed"
+            },
+        ],
+
+        "mainDish": [
+            {
+                "dishName": "Spaghetti",
+                "dishID": "m020001",
+                "dishPrice": 9.60,
+                "dishDescription": "Spaghetti",
+                "dishImageURL": "Spaghetti",
+                "dishImageAlt": "Insert Alt Text here",
+                "dishInfoForWACG": "Placeholder, in Case needed"
+            },
+
+            {
+                "dishName": "Pizza Tonno e Cipolla",
+                "dishID": "m020002",
+                "dishPrice": 8.50,
+                "dishDescription": "Insert Dish description for p Tag here",
+                "dishImageURL": "Insert URL Here",
+                "dishImageAlt": "Insert Alt Text here",
+                "dishInfoForWACG": "Placeholder, in Case needed"
+            },
+        ],
+
+        "dessert": [
+            {
+                "dishName": "Tiramisu",
+                "dishID": "d030001",
+                "dishPrice": 7.60,
+                "dishDescription": "Tiramisu",
+                "dishImageURL": "Tiramisu",
+                "dishImageAlt": "Insert Alt Text here",
+                "dishInfoForWACG": "Placeholder, in Case needed"
+            },
+
+            {
+                "dishName": "Gelato",
+                "dishID": "d030002",
+                "dishPrice": 4.50,
+                "dishDescription": "Insert Dish description for p Tag here",
+                "dishImageURL": "Insert URL Here",
+                "dishImageAlt": "Insert Alt Text here",
+                "dishInfoForWACG": "Placeholder, in Case needed"
+            },
+        ],
+
+        "beverages": [
+            {
+                "dishName": "Malzbier",
+                "dishID": "b040001",
+                "dishPrice": 2.50,
+                "dishDescription": "Bruschetta",
+                "dishImageURL": "Bruschetta",
+                "dishImageAlt": "Insert Alt Text here",
+                "dishInfoForWACG": "Placeholder, in Case needed"
+            },
+
+            {
+                "dishName": "Cola",
+                "dishID": "b040002",
+                "dishPrice": 2.60,
+                "dishDescription": "Insert Dish description for p Tag here",
+                "dishImageURL": "Insert URL Here",
+                "dishImageAlt": "Insert Alt Text here",
+                "dishInfoForWACG": "Placeholder, in Case needed"
+            },
+        ],
+    }
+];
