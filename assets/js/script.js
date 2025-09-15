5c28dbaa4a00f4331095715fce22db1d9a9ce60e
Robert van den Boom <vdboom.robert@gmail.com>
2025-09-15 09:53:40 +0200

    pre work progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,36 @@
 // This document is reserved for the main logic
 
 let basket = [2.5, 4.1, 5.2, 6, 9];
-
+let newBasket = [];
 function calculateTotalSum () {
     let totalSum = 0;
     for (let basketIndex = 0; basketIndex < basket.length; basketIndex++) {
-        totalSum += basket[basketIndex];
+        let total = newBasket[basketIndex].dishPrice;
+        totalSum += total;
         console.log(totalSum);       
     }
 }
 
 
-console.log(dishData.starters[0].dishPrice);
-
-let newBasket = [];
-
 function createNewItemInBasket(category, index) {
-    objectName = dishData[category][index].dishID;
-    let newElement = {["dishID" : objectName, "dishPrice" :  ]};
+    let pointer = dishData[category][index].dishID;
+    let price = dishData[category][index].dishPrice;
+    let newElement = {"dishID" : pointer, "dishPrice" : price, "amount" : 1};
 
     newBasket.push(newElement);
 }
+
 createNewItemInBasket("starters", 2)
+createNewItemInBasket("starters", 1)
 console.log(newBasket);
+
+calculateTotalSum()
+
+function itemPlusOne() {
+
+}
+
+// write function to add item to cart -> check if in cart -> createNew / itemPlusOne
+// look for item in cart
+// only look if cart len != 0 1
+// 
