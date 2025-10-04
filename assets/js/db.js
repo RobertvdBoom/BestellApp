7bc0b255bd4b153f8498c299b17ee18634c16214
Robert van den Boom <vdboom.robert@gmail.com>
2025-10-05 00:21:01 +0200

    updated pictures, updated URL

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,194 +1,194 @@
-let dishData = 
-    {
-        "starters": [
-            {
-                "dishName": "Edamame mit Meersalz",
-                "dishID": "s010001",
-                "dishPrice": 4.20,
-                "dishDescription": "Gedämpfte junge Sojabohnen, leicht gesalzen – ein klassischer japanischer Snack.",
-                "dishImageURL": "/assets/img/starters/edamame.jpg",
-                "dishImageAlt": "Schale mit Edamame und Meersalz",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Gyoza",
-                "dishID": "s010002",
-                "dishPrice": 6.80,
-                "dishDescription": "Hausgemachte japanische Teigtaschen, gefüllt mit Gemüse und Huhn, serviert mit Sojasauce.",
-                "dishImageURL": "/assets/img/starters/gyoza.jpg",
-                "dishImageAlt": "Japanische Gyoza-Teigtaschen auf einem Teller",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Miso Suppe",
-                "dishID": "s010003",
-                "dishPrice": 3.90,
-                "dishDescription": "Traditionelle Suppe aus Miso-Paste mit Tofu, Algen und Frühlingszwiebeln.",
-                "dishImageURL": "/assets/img/starters/miso-soup.jpg",
-                "dishImageAlt": "Miso Suppe mit Tofu und Algen in einer Schale",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Tempura-Gemüse",
-                "dishID": "s010004",
-                "dishPrice": 7.20,
-                "dishDescription": "Knusprig frittiertes Gemüse im leichten Tempurateig, serviert mit Dip-Sauce.",
-                "dishImageURL": "/assets/img/starters/vegetable-tempura.jpg",
-                "dishImageAlt": "Tempura-Gemüse mit Dip-Sauce",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Onigiri",
-                "dishID": "s010005",
-                "dishPrice": 5.10,
-                "dishDescription": "Reisbällchen mit Sesam, Algen und variierenden Füllungen wie Lachs oder Umeboshi.",
-                "dishImageURL": "/assets/img/starters/onigiri.jpg",
-                "dishImageAlt": "Onigiri Reisbällchen mit Nori",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            }
-        ],
+let dishData =
+{
+    "starters": [
+        {
+            "dishName": "Edamame mit Meersalz",
+            "dishID": "s010001",
+            "dishPrice": 4.20,
+            "dishDescription": "Gedämpfte junge Sojabohnen, leicht gesalzen – ein klassischer japanischer Snack.",
+            "dishImageURL": "/assets/img/starters/edamame.jpg",
+            "dishImageAlt": "Schale mit Edamame und Meersalz",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Gyoza",
+            "dishID": "s010002",
+            "dishPrice": 6.80,
+            "dishDescription": "Hausgemachte japanische Teigtaschen, gefüllt mit Gemüse und Huhn, serviert mit Sojasauce.",
+            "dishImageURL": "/assets/img/starters/gyoza.jpg",
+            "dishImageAlt": "Japanische Gyoza-Teigtaschen auf einem Teller",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Miso Suppe",
+            "dishID": "s010003",
+            "dishPrice": 3.90,
+            "dishDescription": "Traditionelle Suppe aus Miso-Paste mit Tofu, Algen und Frühlingszwiebeln.",
+            "dishImageURL": "/assets/img/starters/miso-soup.jpg",
+            "dishImageAlt": "Miso Suppe mit Tofu und Algen in einer Schale",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Tempura-Gemüse",
+            "dishID": "s010004",
+            "dishPrice": 7.20,
+            "dishDescription": "Knusprig frittiertes Gemüse im leichten Tempurateig, serviert mit Dip-Sauce.",
+            "dishImageURL": "/assets/img/starters/vegetable-tempura.jpg",
+            "dishImageAlt": "Tempura-Gemüse mit Dip-Sauce",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Onigiri",
+            "dishID": "s010005",
+            "dishPrice": 5.10,
+            "dishDescription": "Reisbällchen mit Sesam, Algen und variierenden Füllungen wie Lachs oder Umeboshi.",
+            "dishImageURL": "/assets/img/starters/onigiri.jpg",
+            "dishImageAlt": "Onigiri Reisbällchen mit Nori",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        }
+    ],
 
-        "mainDish": [
-            {
-                "dishName": "Ramen Shoyu",
-                "dishID": "m020001",
-                "dishPrice": 12.50,
-                "dishDescription": "Weizennudelsuppe mit klarer Sojasaucen-Brühe, Hühnerfleisch, Ei und Frühlingszwiebeln.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Schüssel mit Ramen in Sojasauce-Brühe",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Katsu Curry",
-                "dishID": "m020002",
-                "dishPrice": 13.90,
-                "dishDescription": "Knusprig paniertes Hähnchenschnitzel mit japanischer Currysauce und Reis.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Japanisches Katsu Curry mit Reis",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Udon Nudelgericht",
-                "dishID": "m020003",
-                "dishPrice": 11.80,
-                "dishDescription": "Dicke Udon-Nudeln in leichter Brühe, serviert mit Gemüse und Tempura-Garnelen.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Schüssel mit Udon Nudeln und Tempura",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Unagi Don",
-                "dishID": "m020004",
-                "dishPrice": 16.50,
-                "dishDescription": "Gegrillter Aal mit süß-salziger Sauce auf einem Bett aus Reis.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Unagi Don – gegrillter Aal auf Reis",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Vegetarisches Sushi-Set",
-                "dishID": "m020005",
-                "dishPrice": 14.20,
-                "dishDescription": "Variation von Sushi mit Avocado, Gurke, Tofu und eingelegtem Gemüse.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Vegetarisches Sushi-Set mit Nigiri und Maki",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            }
-        ],
+    "mainDish": [
+        {
+            "dishName": "Ramen Shoyu",
+            "dishID": "m020001",
+            "dishPrice": 12.50,
+            "dishDescription": "Weizennudelsuppe mit klarer Sojasaucen-Brühe, Hühnerfleisch, Ei und Frühlingszwiebeln.",
+            "dishImageURL": "/assets/img/mainDish/shoyu-ramen.jpg",
+            "dishImageAlt": "Schüssel mit Ramen in Sojasauce-Brühe",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Katsu Curry",
+            "dishID": "m020002",
+            "dishPrice": 13.90,
+            "dishDescription": "Knusprig paniertes Hähnchenschnitzel mit japanischer Currysauce und Reis.",
+            "dishImageURL": "/assets/img/mainDish/katsu-curry.jpg",
+            "dishImageAlt": "Japanisches Katsu Curry mit Reis",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Udon Nudelgericht",
+            "dishID": "m020003",
+            "dishPrice": 11.80,
+            "dishDescription": "Dicke Udon-Nudeln in leichter Brühe, serviert mit Gemüse und Tempura-Garnelen.",
+            "dishImageURL": "/assets/img/mainDish/udon-tempura.jpg",
+            "dishImageAlt": "Schüssel mit Udon Nudeln und Tempura",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Unagi Don",
+            "dishID": "m020004",
+            "dishPrice": 16.50,
+            "dishDescription": "Gegrillter Aal mit süß-salziger Sauce auf einem Bett aus Reis.",
+            "dishImageURL": "/assets/img/mainDish/unagi-don.jpg",
+            "dishImageAlt": "Unagi Don – gegrillter Aal auf Reis",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Vegetarisches Sushi-Set",
+            "dishID": "m020005",
+            "dishPrice": 14.20,
+            "dishDescription": "Variation von Sushi mit Avocado, Gurke, Tofu und eingelegtem Gemüse.",
+            "dishImageURL": "/assets/img/mainDish/vegetable-sushi.jpg",
+            "dishImageAlt": "Vegetarisches Sushi-Set mit Nigiri und Maki",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        }
+    ],
 
-        "dessert": [
-            {
-                "dishName": "Mochi-Eis",
-                "dishID": "d030001",
-                "dishPrice": 5.80,
-                "dishDescription": "Gefüllte Reismehlkugeln mit cremiger Eisfüllung in verschiedenen Geschmacksrichtungen.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Japanisches Mochi-Eis in bunten Farben",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Dorayaki",
-                "dishID": "d030002",
-                "dishPrice": 4.90,
-                "dishDescription": "Fluffige japanische Pfannkuchen gefüllt mit süßer roter Bohnenpaste.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Dorayaki Pfannkuchen mit Bohnenpaste",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Matcha Tiramisu",
-                "dishID": "d030003",
-                "dishPrice": 6.70,
-                "dishDescription": "Japanisch-italienische Fusion aus cremigem Tiramisu mit Matcha-Pulver.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Matcha Tiramisu im Glas",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Yuzu Cheesecake",
-                "dishID": "d030004",
-                "dishPrice": 6.20,
-                "dishDescription": "Frischer Käsekuchen mit einer zitrusartigen Yuzu-Note.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Japanischer Yuzu-Cheesecake",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Kuro Goma Pudding",
-                "dishID": "d030005",
-                "dishPrice": 5.40,
-                "dishDescription": "Cremiger schwarzer Sesampudding mit feinem, nussigem Aroma.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Kuro Goma Pudding in einer Schale",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            }
-        ],
+    "dessert": [
+        {
+            "dishName": "Mochi-Eis",
+            "dishID": "d030001",
+            "dishPrice": 5.80,
+            "dishDescription": "Gefüllte Reismehlkugeln mit cremiger Eisfüllung in verschiedenen Geschmacksrichtungen.",
+            "dishImageURL": "/assets/img/dessert/mochi-eis.jpg",
+            "dishImageAlt": "Japanisches Mochi-Eis in bunten Farben",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Dorayaki",
+            "dishID": "d030002",
+            "dishPrice": 4.90,
+            "dishDescription": "Fluffige japanische Pfannkuchen gefüllt mit süßer roter Bohnenpaste.",
+            "dishImageURL": "/assets/img/dessert/dorayaki.jpg",
+            "dishImageAlt": "Dorayaki Pfannkuchen mit Bohnenpaste",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Yuzu Cheesecake",
+            "dishID": "d030004",
+            "dishPrice": 6.20,
+            "dishDescription": "Frischer Käsekuchen mit einer zitrusartigen Yuzu-Note.",
+            "dishImageURL": "/assets/img/dessert/yuzu-cheesecake.jpg",
+            "dishImageAlt": "Japanischer Yuzu-Cheesecake",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Banh Cam - Sesambällchen aus Vietnam",
+            "dishID": "d030005",
+            "dishPrice": 5.40,
+            "dishDescription": "Knusprig frittierte Sesambällchen mit einer süßen Füllung aus Mungbohnen - ein beliebter vietnamesischer Snack.",
+            "dishImageURL": "/assets/img/dessert/banh-cam.jpg",
+            "dishImageAlt": "Banh Cam - Vietnamesische Sesambällchen auf einem Teller",
+            "dishInfoForWACG": "Frittierte Bällchen mit Mungbohnenfüllung, umhüllt mit Sesam; enthält Gluten und Hülsenfrüchte."
+        },
+        {
+            "dishName": "Matcha Tiramisu",
+            "dishID": "d030003",
+            "dishPrice": 6.70,
+            "dishDescription": "Japanisch-italienische Fusion aus cremigem Tiramisu mit Matcha-Pulver.",
+            "dishImageURL": "/assets/img/dessert/matcha-tiramisu.jpg",
+            "dishImageAlt": "Matcha Tiramisu im Glas",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        }
+    ],
 
-        "beverages": [
-            {
-                "dishName": "Matcha Latte",
-                "dishID": "b040001",
-                "dishPrice": 4.80,
-                "dishDescription": "Cremiger Latte mit fein gemahlenem Matcha-Grüntee.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Matcha Latte in einer Keramikschale",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Genmaicha",
-                "dishID": "b040002",
-                "dishPrice": 3.90,
-                "dishDescription": "Grüntee mit geröstetem Reis, aromatisch und mild.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Genmaicha Tee in einer Tasse",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Pflaumenwein (Umeshu)",
-                "dishID": "b040003",
-                "dishPrice": 6.50,
-                "dishDescription": "Süßlicher japanischer Likör aus eingelegten Pflaumen, kalt serviert.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Glas mit japanischem Pflaumenwein Umeshu",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Hojicha",
-                "dishID": "b040004",
-                "dishPrice": 3.70,
-                "dishDescription": "Gerösteter Grüntee mit mildem, leicht nussigem Geschmack.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Tasse mit Hojicha Tee",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            },
-            {
-                "dishName": "Ramune",
-                "dishID": "b040005",
-                "dishPrice": 3.20,
-                "dishDescription": "Japanisches Erfrischungsgetränk in der traditionellen Glasflasche mit Kugelverschluss.",
-                "dishImageURL": "Insert URL Here",
-                "dishImageAlt": "Ramune Getränkeflasche",
-                "dishInfoForWACG": "Placeholder, in Case needed"
-            }
-        ],
-    };
+    "beverages": [
+        {
+            "dishName": "Matcha Latte",
+            "dishID": "b040001",
+            "dishPrice": 4.80,
+            "dishDescription": "Cremiger Latte mit fein gemahlenem Matcha-Grüntee.",
+            "dishImageURL": "Insert URL Here",
+            "dishImageAlt": "Matcha Latte in einer Keramikschale",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Genmaicha",
+            "dishID": "b040002",
+            "dishPrice": 3.90,
+            "dishDescription": "Grüntee mit geröstetem Reis, aromatisch und mild.",
+            "dishImageURL": "Insert URL Here",
+            "dishImageAlt": "Genmaicha Tee in einer Tasse",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Pflaumenwein (Umeshu)",
+            "dishID": "b040003",
+            "dishPrice": 6.50,
+            "dishDescription": "Süßlicher japanischer Likör aus eingelegten Pflaumen, kalt serviert.",
+            "dishImageURL": "Insert URL Here",
+            "dishImageAlt": "Glas mit japanischem Pflaumenwein Umeshu",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Hojicha",
+            "dishID": "b040004",
+            "dishPrice": 3.70,
+            "dishDescription": "Gerösteter Grüntee mit mildem, leicht nussigem Geschmack.",
+            "dishImageURL": "Insert URL Here",
+            "dishImageAlt": "Tasse mit Hojicha Tee",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        },
+        {
+            "dishName": "Ramune",
+            "dishID": "b040005",
+            "dishPrice": 3.20,
+            "dishDescription": "Japanisches Erfrischungsgetränk in der traditionellen Glasflasche mit Kugelverschluss.",
+            "dishImageURL": "Insert URL Here",
+            "dishImageAlt": "Ramune Getränkeflasche",
+            "dishInfoForWACG": "Placeholder, in Case needed"
+        }
+    ],
+};
