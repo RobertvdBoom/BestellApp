5d1bcb20c2034ca50eb6fc0ded461a877e816a7e
Robert van den Boom <vdboom.robert@gmail.com>
2025-11-05 23:59:50 +0100

    feat: preparation of is In itemBasket

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,75 @@
-// This document is reserved for the main logic
-// RESP MENU
-function toggleRespMenu () {
-    document.getElementById("resp_menu").classList.toggle("resp_menu_closed");
+function toggleRespMenu() {
+  document.getElementById("resp_menu").classList.toggle("resp_menu_closed");
 }
 
-
-let basket = [];
-
-
-function calculateRating(){
-    return averageRating = (rating.totalStars / rating.totalRatings).toFixed(1);
+function calculateRating() {
+  return averageRating = (rating.totalStars / rating.totalRatings).toFixed(1);
 }
 
 let liked = false;
 
 function restaurantRatingPlusMinus() {
-    ratingButtonRef = document.getElementById('ratingButton');
-    if (liked == false) {
-        ratingButtonRef.innerHTML = "-";
-        liked = true;
-        restaurantRating.totalRatings ++;
-        console.log(restaurantRating.totalRatings)
-    } else {
-        ratingButtonRef.innerHTML = "+";
-        liked = false;
-        restaurantRating.totalRatings --;
-        console.log(restaurantRating.totalRatings)
-    }
+  ratingButtonRef = document.getElementById('ratingButton');
+  if (liked == false) {
+    ratingButtonRef.innerHTML = "-";
+    liked = true;
+    restaurantRating.totalRatings++;
+    console.log(restaurantRating.totalRatings)
+  } else {
+    ratingButtonRef.innerHTML = "+";
+    liked = false;
+    restaurantRating.totalRatings--;
+    console.log(restaurantRating.totalRatings)
+  }
 
 }
 
-console.log(newBasket);
-
 function itemPlusOne(index) {
-    inventory[index].quantity ++;
-    console.log(inventory[index].quantity);
-    renderAmount(index);
+  inventory[index].quantity++;
+  console.log(inventory[index].quantity);
+  renderAmount(index);
 }
 
 function itemMinusOne(index) {
-    inventory[index].quantity --;
-    console.log(inventory[index].quantity);
-    if (inventory[index].quantity !== 0) {
-      renderAmount(index);
-    }
-    else {
-      inventory.splice(index, 1);
-      console.log(inventory[index]);
-    }
+  inventory[index].quantity--;
+  console.log(inventory[index].quantity);
+  if (inventory[index].quantity !== 0) {
+    renderAmount(index);
+  }
+  else {
+    inventory.splice(index, 1);
+    console.log(inventory[index]);
+  }
 }
 
 function pushItemToBasket(category, index) {
-    let itemObjectInBasket = { 
-      "category" : category,
-      "itemName" : dishData[category][index].dishName,
-      "dishPrice" : dishData[category][index].dishPrice,
-      "dishAmount" : 1,
+  if (itemIsInBasket == true) {
+    findIndexInBasket(category, index);
+  } else {
+    let itemObjectInBasket = {
+      "category": category,
+      "itemName": dishData[category][index].dishName,
+      "dishPrice": dishData[category][index].dishPrice,
+      "dishAmount": 1,
     }
     basket.push(itemObjectInBasket);
+  }
+
+
 }
 
 pushItemToBasket('starters', 0)
 
 
 function isInBasket(item) {
-    // Pass name
-    // check if its in basket 
-    // return index
-    // basket[index] ++
-    
-  }
+  // Pass name
+  // check if its in basket 
+  // return index
+  // basket[index] ++
 
-  function renderAmount(index) {
+}
 
-  }
+function renderAmount(index) {
+
+}
 
