edf04eb74705a1148a1c5a0f5f3b5cb7c720e5bc
Robert van den Boom <vdboom.robert@gmail.com>
2026-03-15 00:55:02 +0100

    style: additional handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,42 +13,6 @@ function init() {
   openIntroDialog();
 }
 
-let introDialog = true;
-
-function toggleSRBox() {
-  let srBoxRef = document.getElementById('basket-status').classList.toggle('sr-announcement');
-}
-
-function openIntroDialog() {
-  if (introDialog === true) {
-    document.getElementById('intro-dialog').showModal();
-  }
-}
-
-function closeIntroDialog() {
-  document.getElementById('intro-dialog').close();
-}
-
-function dontShowIntroAgain() {
-  introDialog = false;
-  saveIntroInfoInLS();
-  closeIntroDialog();
-}
-
-function showIntroAgain() {
-  introDialog = true;
-  saveIntroInfoInLS();
-  openIntroDialog();
-}
-
-function saveIntroInfoInLS() {
-    localStorage.setItem('introDialogOption', JSON.stringify(introDialog));
-}
-
-function fetchIntroInfoFromLS() {
-    introDialog = JSON.parse(localStorage.getItem("introDialogOption"));
-}
-
 function activateCategoryActiveBorder(category) {
   let oldButtonContentRef = document.querySelectorAll(`.${activeCategory}`);
   let newButtonContentRef = document.querySelectorAll(`.${category}`);
--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,1 +1,37 @@
 // insert info dialog with option to not show again on reload. Base this on bool -> add button to not show again on reload (saved to local storage  )
+
+let introDialog = true;
+
+function toggleSRBox() {
+  document.getElementById('basket-status').classList.toggle('sr-announcement');
+}
+
+function openIntroDialog() {
+  if (introDialog === true) {
+    document.getElementById('intro-dialog').showModal();
+  }
+}
+
+function closeIntroDialog() {
+  document.getElementById('intro-dialog').close();
+}
+
+function dontShowIntroAgain() {
+  introDialog = false;
+  saveIntroInfoInLS();
+  closeIntroDialog();
+}
+
+function showIntroAgain() {
+  introDialog = true;
+  saveIntroInfoInLS();
+  openIntroDialog();
+}
+
+function saveIntroInfoInLS() {
+    localStorage.setItem('introDialogOption', JSON.stringify(introDialog));
+}
+
+function fetchIntroInfoFromLS() {
+    introDialog = JSON.parse(localStorage.getItem("introDialogOption"));
+}
